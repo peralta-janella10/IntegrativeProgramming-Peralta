700b0d00154033630eff352fb94cd1e445e5d710
Janella Peralta <peraltajanella1@gmail.com>
2026-03-01 00:21:11 +0800

    Add files via upload

---
+++ StudentGradeApplication/StudentGradeApplication/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace StudentGradeApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.namebox = new System.Windows.Forms.TextBox();
+            this.englishbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.mathbox = new System.Windows.Forms.TextBox();
+            this.sciencebox = new System.Windows.Forms.TextBox();
+            this.filipinobox = new System.Windows.Forms.TextBox();
+            this.historybox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.genbox = new System.Windows.Forms.Button();
+            this.display = new System.Windows.Forms.Label();
+            this.dis = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(46, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // namebox
+            // 
+            this.namebox.Location = new System.Drawing.Point(108, 50);
+            this.namebox.Name = "namebox";
+            this.namebox.Size = new System.Drawing.Size(201, 20);
+            this.namebox.TabIndex = 1;
+            // 
+            // englishbox
+            // 
+            this.englishbox.Location = new System.Drawing.Point(118, 109);
+            this.englishbox.Name = "englishbox";
+            this.englishbox.Size = new System.Drawing.Size(72, 20);
+            this.englishbox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(104, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Grade";
+            // 
+            // mathbox
+            // 
+            this.mathbox.Location = new System.Drawing.Point(118, 135);
+            this.mathbox.Name = "mathbox";
+            this.mathbox.Size = new System.Drawing.Size(72, 20);
+            this.mathbox.TabIndex = 4;
+            // 
+            // sciencebox
+            // 
+            this.sciencebox.Location = new System.Drawing.Point(118, 161);
+            this.sciencebox.Name = "sciencebox";
+            this.sciencebox.Size = new System.Drawing.Size(72, 20);
+            this.sciencebox.TabIndex = 5;
+            // 
+            // filipinobox
+            // 
+            this.filipinobox.Location = new System.Drawing.Point(118, 187);
+            this.filipinobox.Name = "filipinobox";
+            this.filipinobox.Size = new System.Drawing.Size(72, 20);
+            this.filipinobox.TabIndex = 6;
+            // 
+            // historybox
+            // 
+            this.historybox.Location = new System.Drawing.Point(118, 213);
+            this.historybox.Name = "historybox";
+            this.historybox.Size = new System.Drawing.Size(72, 20);
+            this.historybox.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(46, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 21);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "English";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(46, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 21);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Math";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Location = new System.Drawing.Point(46, 161);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 21);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Science";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label6.Location = new System.Drawing.Point(46, 187);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 21);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Filipino";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label7.Location = new System.Drawing.Point(46, 213);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 21);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "History";
+            // 
+            // genbox
+            // 
+            this.genbox.Location = new System.Drawing.Point(65, 257);
+            this.genbox.Name = "genbox";
+            this.genbox.Size = new System.Drawing.Size(94, 51);
+            this.genbox.TabIndex = 13;
+            this.genbox.Text = "Generate Average";
+            this.genbox.UseVisualStyleBackColor = true;
+            this.genbox.Click += new System.EventHandler(this.genbox_Click);
+            // 
+            // display
+            // 
+            this.display.AutoSize = true;
+            this.display.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.display.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.display.Location = new System.Drawing.Point(237, 109);
+            this.display.Name = "display";
+            this.display.Size = new System.Drawing.Size(0, 21);
+            this.display.TabIndex = 14;
+            // 
+            // dis
+            // 
+            this.dis.AutoSize = true;
+            this.dis.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dis.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.dis.Location = new System.Drawing.Point(210, 109);
+            this.dis.Name = "dis";
+            this.dis.Size = new System.Drawing.Size(0, 21);
+            this.dis.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(636, 344);
+            this.Controls.Add(this.dis);
+            this.Controls.Add(this.display);
+            this.Controls.Add(this.genbox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.historybox);
+            this.Controls.Add(this.filipinobox);
+            this.Controls.Add(this.sciencebox);
+            this.Controls.Add(this.mathbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.englishbox);
+            this.Controls.Add(this.namebox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox namebox;
+        private System.Windows.Forms.TextBox englishbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox mathbox;
+        private System.Windows.Forms.TextBox sciencebox;
+        private System.Windows.Forms.TextBox filipinobox;
+        private System.Windows.Forms.TextBox historybox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button genbox;
+        private System.Windows.Forms.Label display;
+        private System.Windows.Forms.Label dis;
+    }
+}
+
