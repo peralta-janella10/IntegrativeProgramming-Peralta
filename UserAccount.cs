4a80d64193dbb78cdfb415b32636ca700be4fde1
Janella Peralta <peraltajanella1@gmail.com>
2026-05-23 19:35:43 +0800

    Add files via upload

---
+++ UserAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashierApplication
+{
+    internal class UserAccount
+    {
+        private string full_name;
+        protected string user_name;
+        protected string user_password;
+
+        public UserAccount(string name, string Uname, string password)
+        {
+            full_name = name;
+            user_name = Uname;
+            user_password = password;
+        }
+        public bool validateLogin(string Uname, string password)
+        {
+            return user_name == Uname && user_password == password;
+        }
+        public string getFullName()
+        {
+            return full_name;
+        }
+    }
+}
